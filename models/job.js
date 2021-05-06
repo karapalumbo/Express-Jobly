5275a9dd6ad791ca16c7a0da203f7106cbed143c
Kara <palumbokara@yahoo.com>
2021-05-06 11:45:57 -0600

    adding job model

---
+++ models/job.js
@@ -0,0 +1,133 @@
+"use strict";
+
+const db = require("../db");
+const { BadRequestError, NotFoundError, ExpressError } = require("../expressError");
+const { sqlForPartialUpdate } = require("../helpers/sql");
+
+
+
+class Job {
+
+  static async create({ title, salary, equity, company_handle }) {
+    const duplicateCheck = await db.query(
+          `SELECT title
+           FROM jobs
+           WHERE title = $1`,
+        [title]);
+
+    if (duplicateCheck.rows[0])
+      throw new BadRequestError(`Duplicate job: ${title}`);
+
+    const result = await db.query(
+          `INSERT INTO jobs
+           (title, salary, equity, company_handle)
+           VALUES ($1, $2, $3, $4)
+           RETURNING title, salary, equity, company_handle AS "companyHandle"`,
+        [
+          title,
+          salary,
+          equity,
+          companyHandle,
+        ],
+    );
+    const job = result.rows[0];
+
+    return job;
+  }
+
+
+  static async findAll(searchFilters = {}) { 
+      let query =`SELECT id, 
+                  title, 
+                  salary, 
+                  equity, 
+                  company_handle AS "companyHandle"
+           FROM jobs`;
+    
+    let whereExpressions = []; 
+    let queryValues = []; 
+
+    const { title, minSalary, hasEquity } = searchFilters;
+
+    if (title !== undefined) {
+      queryValues.push(`%${title}%`);
+      whereExpressions.push(`title ILIKE $${queryValues.length}`);
+    }
+
+    if (minSalary !== undefined) {
+      queryValues.push(minSalary);
+      whereExpressions.push(`salary >= $${queryValues.length}`);
+    }
+
+    if (hasEquity === true) {
+      queryValues.push(hasEquity);
+      whereExpressions.push(`equity > 0`);
+    }
+
+    if (whereExpressions.length > 0) {
+      query += " WHERE " + whereExpressions.join(" AND ");
+    }
+
+    query += " ORDER BY title";
+
+    let jobsRes = await db.query(query, queryValues);
+    return jobsRes.rows;
+  }
+
+
+  static async get(id) {
+    const jobRes = await db.query(
+          `SELECT id,
+                  title,
+                  salary,
+                  equity,
+                  company_handle AS "companyHandle",
+           FROM jobs
+           WHERE id = $1`,
+        [id]);
+
+    const job = jobRes.rows[0];
+
+    if (!job) throw new NotFoundError(`No job: ${id}`);
+
+    return job;
+  }
+
+
+  static async update(id, data) {
+    const { setCols, values } = sqlForPartialUpdate(
+        data,
+        {});
+    const idVarIdx = "$" + (values.length + 1);
+
+    const querySql = `UPDATE jobs 
+                      SET ${setCols} 
+                      WHERE id = ${idVarIdx} 
+                      RETURNING id, 
+                                title, 
+                                salary, 
+                                company_handle AS "companyHandle"`;
+    const result = await db.query(querySql, [...values, id]);
+    const job = result.rows[0];
+
+    if (!job) throw new NotFoundError(`No job: ${id}`);
+
+    return job;
+  }
+
+
+  static async remove(id) {
+    const result = await db.query(
+          `DELETE
+           FROM jobs
+           WHERE id = $1
+           RETURNING id`,
+        [id]);
+    const job = result.rows[0];
+
+    if (!job) throw new NotFoundError(`No job: ${id}`);
+  }
+}
+
+
+module.exports = Job;
